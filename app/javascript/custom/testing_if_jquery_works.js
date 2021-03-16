227bd0a0fd844de1b327df5577d64040046e4046
mcndev <mcnikkopinlacnetflix@gmail.com>
2021-03-16 23:47:06 +0800

    Working on views

---
+++ app/javascript/custom/testing_if_jquery_works.js
@@ -0,0 +1,13 @@
+// jQuery(window).on('load', function () {
+//     $('.preloader').fadeOut();
+// });
+
+// window.onload = function() {
+//     if (window.$) {  
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,21 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'bootstrap'
+import 'jquery'
+import '../custom/testing_if_jquery_works.js'
+import '../stylesheets/application'
+import '../stylesheets/normalize'
+import '../stylesheets/cover'
+import '../stylesheets/signin'
+
+document.addEventListener('turbolinks:load', () => {
+     $('[data-toggle="tooltip"]').tooltip()
+     $('[data-toggle="popover"]').popover()
+})
